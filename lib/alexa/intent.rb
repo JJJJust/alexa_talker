c6e1f2aa3c7cef930cc33c9391012b622d319b2b
Justin S. Johnson <JSJohnson@cedarsonic.org>
2019-03-23 22:26:08 -0400

    Move Slot to Intent class

--- lib/alexa/intent.rb
+++ lib/alexa/intent.rb
@@ -1,13 +1,29 @@
 # frozen_string_literal: true
 
+require_relative 'slot'
+
 module Alexa
   class Intent
     attr_reader :name
     attr_reader :confirmation_status
+    attr_reader :slots
 
     def initialize(hash)
       @name = hash[:name]
       @confirmation_status = hash[:confirmationStatus]
+      @slots = slot_handler(hash[:slots]) if hash.key?(:slots)
+    end
+
+    private
+
+    def slot_handler(hash)
+      return [] if hash.empty?
+
+      result = []
+      hash.each do |_k, v|
+        result << Alexa::Slot.new(v)
+      end
+      result
     end
   end
 end
--- lib/alexa/request/intent_request.rb
+++ lib/alexa/request/intent_request.rb
@@ -3,7 +3,6 @@
 require_relative '../request'
 require_relative '../session'
 require_relative '../intent'
-require_relative '../slot'
 
 module Alexa
   class Request
@@ -12,26 +11,12 @@ module Alexa
       attr_reader :dialog_state
       attr_reader :confirmation_status
       attr_reader :intent
-      attr_reader :slots
 
       def initialize(hash)
         super
         @session = Alexa::Session.new(hash[:session])
         @dialog_state = hash[:request][:dialogState]
         @intent = Alexa::Intent.new(hash[:request][:intent])
-        @slots = slot_handler(hash[:request][:intent][:slots])
-      end
-
-      private
-
-      def slot_handler(hash)
-        result = []
-        unless hash.empty?
-          hash.each do |_k, v|
-            result << Alexa::Slot.new(v)
-          end
-        end
-        result
       end
     end
   end
